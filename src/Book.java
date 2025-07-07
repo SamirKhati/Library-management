2645873d96bfad59ae42d791923c3ce065eb897d
SamirKhati <Samirpariyar588@gmail.com>
2025-07-07 12:15:01 +0545

    started

---
+++ src/Book.java
@@ -0,0 +1,15 @@
+public class Book {
+    //attributes-> book number, nam, quantity, author
+    //TODO create the attributes
+    int bookNo;
+    String bookName;
+    int bookQuantity;
+    String bookAuthor;
+    //behaviour -> increase quantity, decrease quantity
+    public void increaseQuantity(){
+        System.out.println("need work to increase");
+    }
+    public void decreaseQuantity() {
+        System.out.println("need work to decrease");
+    }
+}
---
+++ src/User.java
@@ -0,0 +1,17 @@
+public class User {
+    //attributes -> name, username, contact
+    String Name;
+    String username;
+    int contact;
+    //behaviour -> check is return
+    public void isRegistered(){
+        System.out.println("need to work on checking register");
+    }
+    public void borrowBook(){
+        System.out.println("need to work on borrow book");
+    }
+    public void returnBook(){
+        System.out.println("need to work on return book");
+
+    }
+}
