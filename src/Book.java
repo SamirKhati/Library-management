57e00d483eb85086e082ec1eb0a8cb8073c41cc5
SamirKhati <Samirpariyar588@gmail.com>
2025-07-10 12:57:33 +0545

    database connection

--- src/Book.java
+++ src/Book.java
@@ -1,55 +1,51 @@
-public class Book {
-    //attributes-> book number, nam, quantity, author
-    // create the attributes
-    private int BookNo;
-    private String BookName;
-    private int BookQuantity;
-    private String BookAuthor;
-//    private int DecreaseQuantity;
-    public Book(int BookNo, String BookName, int BookQuantity, String BookAuthor){
-        this.BookNo = BookNo;
-        this.BookAuthor = BookAuthor;
-        this.BookName=BookName;
-        this.BookQuantity=BookQuantity;
-//        this.DecreaseQuantity=DecreaseQuantity;
-    }
-    //behaviour -> increase quantity, decrease quantity
-    public void increaseQuantity(){
-        System.out.println("need work to increase");
-    }
-    public void decreaseQuantity() {
-        System.out.println("need work to decrease");
-    }
 
-    public void setBookNo(int bookNo) {
-        this.BookNo = bookNo;
-    }
 
-    public int getBookNo() {
-        return BookNo;
-    }
+import java.awt.print.Book;
+import java.util.Scanner;
 
-    public void setBookName(String bookName) {
-        this.BookName = bookName;
-    }
+public class Library {
+    public static void main(String[] args) {
+        // TODO create some books and users
+        System.out.println("Books Part!!");
+        Book book1 = new Book(201, "Book 1", 34, "Author 1");
+        System.out.println(book1.getBookNumber());
+        System.out.println(book1.getBookAuthor());
 
-    public String getBookName() {
-        return BookName;
-    }
 
-    public void setBookQuantity(int bookQuantity) {
-        this.BookQuantity = bookQuantity;
-    }
+        System.out.println("\nUser Part!!!");
+        User user1 = new User("Name1", "UserName1", 123456);
+        System.out.println(user1.getName());
 
-    public int getBookQuantity() {
-        return BookQuantity;
-    }
+        System.out.println("Book Part!!!");
+        BookRecord record = new BookRecord(user1, book1);
 
-    public void setBookAuthor(String bookAuthor) {
-        this.BookAuthor = bookAuthor;
-    }
+        // show options available: books, borrow books, return book, exit
+        Scanner scan = new Scanner(System.in);
+        System.out.println("\nWelcome to the library");
+        System.out.println("Enter 1: Show available Books");
+        System.out.println("Enter 2: Borrow Books");
+        System.out.println("Enter 3: Return Books");
+        System.out.println("Enter 4: Exit");
+        System.out.print("Choose an option: ");
+        int option = scan.nextInt();
 
-    public String getBookAuthor() {
-        return BookAuthor;
+
+        if (option == 1) {
+            System.out.println("These Books are Available");
+        } else if (option == 2) {
+            System.out.println("Borrowing Books");
+            // TODO which book to borrow
+            // TODO borrow garera saki sake pachi message print garna parxa
+
+            user1.borrowBook(book1, user1);
+        } else if (option == 3) {
+            System.out.println("Returning Books");
+        } else if (option == 4) {
+            System.out.println("Exiting...");
+        } else {
+            System.out.println("Invalid Input. Please Enter The Available Options");
+        }
+
+        // TODO need ot figure out where to keep the Books?
     }
 }
--- src/BookRecord.java
+++ src/BookRecord.java
@@ -1,32 +1,20 @@
+import java.awt.print.Book;
+
 public class BookRecord {
     private User user;
     private Book book;
     private String date;
-    private boolean isReturned;
+    private boolean returnStatus;
 
     public BookRecord(User user, Book book) {
         this.user = user;
         this.book = book;
-        this.date = "date";//todo jati bela add gareko ho record tyo date hunu parxa
-        this.isReturned = false;
+        this.date = "date"; // TODO jati bela record garyo tetibela ko date auto set huna paryo
+        this.returnStatus = false;
     }
 
-    public void setUser(User user) {
-        this.user = user;
-    }
 
-    public void setBook(Book book) {
-        this.book = book;
-    }
-
-    public void setDate(String date) {
-        this.date = date;
-    }
-
-    public void setReturned(boolean returned) {
-        isReturned = returned;
-    }
-
+    // <==================Getters===============>
     public User getUser() {
         return user;
     }
@@ -39,7 +27,23 @@ public class BookRecord {
         return date;
     }
 
-    public boolean isReturned() {
-        return isReturned;
+    public boolean isReturnStatus() {
+        return returnStatus;
+    }
+    // <===========Setters============>
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public void setReturnStatus(boolean returnStatus) {
+        this.returnStatus = returnStatus;
     }
 }
